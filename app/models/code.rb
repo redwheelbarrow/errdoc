dbdea9215c6027069b1cb9f24806e7a9d5f99c0d
mike <mike@black-stallion.(none)>
2013-06-18 12:59:06 -0600

    Base

---
+++ app/models/code.rb
@@ -0,0 +1,3 @@
+class Code < ActiveRecord::Base
+  attr_accessible :category, :description, :number
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ErrorCodes::Application.config.secret_token = 'fc78613f5cee2de62ac62f0f0f47757ac57cec9fbc0fef08f750591b73c17fa154c4263a1ec4ce72d95405f0037e34132c8f8db5519adce2a03928298f6dfba9'
